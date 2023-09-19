833e6cc3dc5f26b45b80dac581cda176190b3ac5
bharatchandran <bharatchandrankr@gmail.com>
2023-09-19 16:45:05 +0530

    initial commit with login and signup page also backend for signup

---
+++ src/models/userModel.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose"
+
+const userSchema = new mongoose.Schema({
+    username: { 
+        type: String, 
+        required: [true, "Please provide a username"], 
+        unique: true },
+        password: { 
+            type: String,
+            required: [true, "Please provide a password"],
+})
+
+const User = mongoose.models.users || mongoose.model("User", userSchema);
+
+export default User;
